3f0c21bcc8725a1d62cf815b3db05beb9d41b6be
mohittumram <mohittumram1@gmail.com>
2024-06-29 03:37:17 +0530

    connected database

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Sever is running on port", port);
+});
